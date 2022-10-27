f6ef3e1402b4930e67ed8ca1d84faf3fa3310743
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2022-10-27 08:29:06 -0700

    refactor: moved error headers into class static methods
    Previously, I split this into a function outside of the class because
    otherwise in order to use the error messages in the main code required a
    reference to self by something like
    
    `message.bundle_message(message, input_args)`
    
    This commit neatly solves that problem.

--- src/console_messages/bundler.py
+++ src/console_messages/bundler.py
@@ -2,40 +2,36 @@
 
 from .color_control import color
 
-def _Bundler_ErrorHeader():
-    # I split this into a function outside of the class because otherwise in order to use
-    #   the error messages in the main code required a reference to self by doing 
-    #   something like this:
-    #   
-    #   message.bundle_message(message, input_args)
-    return str(color.FAIL + "Bundler error: " + color.ENDC)
-
-
 class BundlerMessages:
     """Consolidates and prints formatted console messages for the bundler functions."""
+
+    @staticmethod
+    def _ErrorHeader():
+        return str(color.FAIL + "Bundler error: " + color.ENDC)
 
     def complete(output_location: str):
         print(color.CONTROL + "Bundling completed." + color.ENDC
         + " Bundled add-on located at: " + color.OKGREEN + output_location + color.ENDC)
     
     def invalid_source_not_a_list(source_input: str):
-        print(_Bundler_ErrorHeader() + "Source file argument must be a list. Unable to process: " + color.WARNING
-            + str(source_input) + color.ENDC + ".")
+        print(BundlerMessages._ErrorHeader() + "Source file argument must be a list. Unable to process: " 
+            + color.WARNING + str(source_input) + color.ENDC + ".")
 
     def invalid_source_empty_list():
-        print(_Bundler_ErrorHeader() + "Source files cannot be an empty list.")
+        print(BundlerMessages._ErrorHeader() + "Source files cannot be an empty list.")
 
     def file_does_not_exist(dir_path: str):
-        print(_Bundler_ErrorHeader() + "The file or folder '" + color.WARNING +  str(dir_path) + color.ENDC 
-            + "' does not exist.")
+        print(BundlerMessages._ErrorHeader() + "The file or folder '" + color.WARNING +  str(dir_path)
+            + color.ENDC + "' does not exist.")
     
     def output_folder_does_not_exist(dir_path: str):
-        print(_Bundler_ErrorHeader() + "The desired output folder '" + color.WARNING +  str(dir_path) + color.ENDC 
-            + "' is either not a folder or does not exist.")
+        print(BundlerMessages._ErrorHeader() + "The desired output folder '" + color.WARNING +  str(dir_path) 
+            + color.ENDC + "' is either not a folder or does not exist.")
     
     def output_file_name_too_long():
-        print(_Bundler_ErrorHeader() + "The output file name must be 189 characters or less.")
+        print(BundlerMessages._ErrorHeader() + "The output file name must be 189 characters or less.")
     
     def bundle_already_exists(bundle_path: str):
-        print(_Bundler_ErrorHeader() + "The desired bundle path '" + color.WARNING +  str(bundle_path) + color.ENDC 
+        print(BundlerMessages._ErrorHeader() + "The desired bundle path '" 
+            + color.WARNING +  str(bundle_path) + color.ENDC 
             + "' already exists. The bundler will not overwrite this file unless the `overwrite` value is True.")
--- src/console_messages/directory_monitor.py
+++ src/console_messages/directory_monitor.py
@@ -6,17 +6,12 @@ This makes the code in src/directory_monitor.py easier to read and maintain.
 
 from .color_control import color
 
-def _DirectoryMonitor_ErrorHeader():
-    # I split this into a function outside of the class because otherwise in order to use
-    #   the error messages in the main code required a reference to self by doing 
-    #   something like this:
-    #   
-    #   message.file_or_folder_does_not_exist(message, file_or_folder_path)
-    return str(color.FAIL + "DirectoryMonitor error: " + color.ENDC)
-
-
 class DirectoryMonitorMessages:
     """Consolidates and prints formatted console messages for the DirectoryMonitor class."""
+
+    @staticmethod
+    def _ErrorHeader():
+        return str(color.FAIL + "DirectoryMonitor error: " + color.ENDC)
 
     def watch():
         print(color.CONTROL + "Watching for updates..." + color.ENDC)
@@ -28,12 +23,12 @@ class DirectoryMonitorMessages:
         print(color.CONTROL + "Monitoring is already active." + color.ENDC + " Continuing to monitor...")
     
     def invalid_directory(directory):
-        print(_DirectoryMonitor_ErrorHeader() + "'DirectoryMonitor.directory' is invalid. Unable to monitor until "
+        print(DirectoryMonitorMessages._ErrorHeader() + "'DirectoryMonitor.directory' is invalid. Unable to monitor until "
             + "provided a valid file or folder path.\n"
             + "    Current bad DirectoryMonitor.directory: " + color.WARNING + str(directory) + color.ENDC)
     
     def file_or_folder_does_not_exist(dir_path):
-        print(_DirectoryMonitor_ErrorHeader() + "The file or folder '" + color.WARNING +  dir_path + color.ENDC 
+        print(DirectoryMonitorMessages._ErrorHeader() + "The file or folder '" + color.WARNING +  dir_path + color.ENDC 
             + "' does not exist.")
     
     def found_deleted_files(num_files):
@@ -48,14 +43,14 @@ class DirectoryMonitorMessages:
         print("DirectoryMonitor found an updated file: " + color.OKGREEN +  filepath + color.ENDC)
     
     def unable_to_unsubscribe(script):
-        print(_DirectoryMonitor_ErrorHeader() + "Unable to unsubscribe '" + color.WARNING + script + color.ENDC 
+        print(DirectoryMonitorMessages._ErrorHeader() + "Unable to unsubscribe '" + color.WARNING + script + color.ENDC 
             + "'. This script was never registered.")
     
     def unable_to_change_directory(new_dir, current_dir):
-        print(_DirectoryMonitor_ErrorHeader() + "'DirectoryMonitor.directory' must be a file or folder that exists.\n"
+        print(DirectoryMonitorMessages._ErrorHeader() + "'DirectoryMonitor.directory' must be a file or folder that exists.\n"
             + "    You tried: " + color.WARNING + str(new_dir) + color.ENDC + "\n"
             + "    Maintaining the current directory at: '" + color.OKGREEN + str(current_dir) + color.ENDC + "'")
     
     def invalid_polling_delay(delay):
-        print(_DirectoryMonitor_ErrorHeader() + "'DirectoryMonitor.polling_delay' must be a number greater than 0.\n"
+        print(DirectoryMonitorMessages._ErrorHeader() + "'DirectoryMonitor.polling_delay' must be a number greater than 0.\n"
             + "    Maintaining poll interval at : " + color.OKGREEN + str(delay) + color.ENDC + " seconds.")
