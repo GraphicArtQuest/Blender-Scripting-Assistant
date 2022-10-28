b776dd43835ff477ba2741c9c0c3c22589f615cf
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2022-10-28 07:59:09 -0700

    feat: create an operator to help toggle the terminal on and off
    Of note, apparently the terminal is only available on Windows machines. I
    am not sure what this code will do on Linux or MacOS.

---
+++ src/operators/toggle_blender_terminal.py
@@ -0,0 +1,10 @@
+import bpy
+
+class ToggleBlenderTerminal(bpy.types.Operator):
+    bl_idname = "debugger.toggle_terminal"
+    bl_label = "Toggle: show or hide the system terminal"
+    bl_description = "Toggles the system terminal on or off"
+
+    def execute(self, context):
+        bpy.ops.wm.console_toggle()
+        return {'FINISHED'}
