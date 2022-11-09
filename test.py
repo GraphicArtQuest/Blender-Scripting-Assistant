a4b6f9769a22e8d50f6d9f4c1d7f91e767bf93d8
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2022-11-09 08:59:52 -0800

    refactor: eliminated redundant test code
    This code had previously been incorporated into the stand alone test file
    `testdebugpy.py` and was no longer needed here.

--- test.py
+++ test.py
@@ -4,12 +4,5 @@ import unittest
 from tests.test_directory_monitor import TestDirectoryMonitor
 from tests.test_bundler import TestBundler
 
-# debugpy.listen(("0.0.0.0", 5678))
-# print("debugpy version is: " + debugpy.__version__)
-# print("Waiting")
-# debugpy.wait_for_client()
-# print("Connected")
-# input('Press Enter to Exit')
-
 if __name__ == '__main__':
     unittest.main()
