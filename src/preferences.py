700fb7a49426b4b076ef15a5f0ceb6557d46dad2
M-Scott-Lassiter <m.s.lassiter@gmail.com>
2024-05-01 11:22:01 -0700

    refactor: added a type ignore comment for use in VS Code editor environment

--- src/preferences.py
+++ src/preferences.py
@@ -13,35 +13,35 @@ class DebuggerPreferences(bpy.types.AddonPreferences):
         name="Location of debugpy (site-packages folder)",
         subtype='DIR_PATH',
         default=check_for_debugpy()
-    )
+    ) # type: ignore
 
     debugpy_timeout: bpy.props.IntProperty(
         name="Timeout",
         min=0,
         default=20
-    )
+    ) # type: ignore
 
     debugpy_port: bpy.props.IntProperty(
         name="Port",
         min=0,
         max=65535,
         default=5678
-    )
+    ) # type: ignore
 
     monitor_path: bpy.props.StringProperty(
         name="File or Folder to Debug",
         subtype="FILE_PATH",
         default= ""
-    )
+    ) # type: ignore
     
     monitor_addon_name: bpy.props.StringProperty(
         name="Name of Monitored Add-on",
         subtype='FILE_PATH',
         default= "Blender Scripting Assistant"
-    )
+    ) # type: ignore
 
     monitor_addon_filename: bpy.props.StringProperty(
         name="File Name of Monitored Add-on",
         default= "blender-scripting-assistant",
         subtype='FILE_PATH',
-    )
+    ) # type: ignore
